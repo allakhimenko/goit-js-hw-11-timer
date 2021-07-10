51b09ae9e884a5928855d004b7d4405a9e3ae765
allakhimenko <all.khimenko24@gmail.com>
2021-07-10 18:56:05 +0300

    commit

--- index copy.js
+++ index copy.js
@@ -1,9 +1,9 @@
 const refs = {
     timerIndicator: document.querySelector('#timer-1'),
-    dataValue: document.querySelector('span[data-value="days"]'),
-    hourValue: document.querySelector('span[data-value="hours"]'),
-    minValue: document.querySelector('span[data-value="mins"]'),
-    secValue: document.querySelector('span[data-value="secs"]'),
+    days: document.querySelector('span[data-value="days"]'),
+    hours: document.querySelector('span[data-value="hours"]'),
+    mins: document.querySelector('span[data-value="mins"]'),
+    secs: document.querySelector('span[data-value="secs"]'),
 };
 
 class CountdownTimer {
@@ -26,7 +26,6 @@ class CountdownTimer {
         const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
         const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
         const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
         return { days, hours, mins, secs };
     };
 
@@ -35,16 +34,16 @@ class CountdownTimer {
     };
 
     updateTimer({ days, hours, mins, secs }) {
-        refs.dataValue.textContent = `${days}`;
-        refs.hourValue.textContent = `${hours}`;
-        refs.minValue.textContent = `${mins}`;
-        refs.secValue.textContent = `${secs}`;
+        refs.days.textContent = `${days}`;
+        refs.hours.textContent = `${hours}`;
+        refs.mins.textContent = `${mins}`;
+        refs.secs.textContent = `${secs}`;
     };
-};  
+};
 
 const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Feb 5, 2022'),
-  });
+  selector: '#timer-1',
+  targetDate: new Date('Jul 31, 2021'),
+});
 
-  timer.start();
+timer.start();
